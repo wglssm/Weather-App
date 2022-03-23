6baff9128993224dffcedf90eba46cfc1c9b96df
Lesi Wei <wglssm.wls@gmail.com>
2022-03-24 01:54:00 +1030

    feat: get weather data from api

--- src/components/CurrentDay/CurrentDay.jsx
+++ src/components/CurrentDay/CurrentDay.jsx
@@ -3,13 +3,35 @@ import React from 'react';
 import locationIcon from './assets/location-pin.png';
 import styles from './CurrentDay.module.css';
 
-const CurrentDay = () => (
+const CurrentDay = ({
+    weekday, 
+    date, 
+    location, 
+    temperature, 
+    weatherIcon, 
+    weatherDescription}) => (
     <div className="d-flex">
         <div className={styles.img}></div>
         <div className={styles.gradient}></div>
         <div className={`${styles.cardInner} d-flex flex-column justify-content-between pt-3 pb-2 pl-2`}>
-            <div></div>
-            <div></div>
+            <div>
+              <h2 className="font-weight-blod mb1"> {weekday} </h2>
+              <p className="mb-0"> { date } </p>
+              <p className="d-flex align-items-baseline font-weight-lighter mb1">
+                <img width="10" 
+                height="15" 
+                src={ locationIcon } 
+                alt="location pin"/>
+              </p>
+              <span> { location } </span>
+            </div>
+            <div>
+              <img width="45" src={weatherIcon} alt="current weather icon"/>
+              <h2 className="font-weight-blod mb-1">
+                <span> { temperature } </span> °C
+              </h2>
+              <h5 className="font-weight-lighter">{weatherDescription} </h5>
+            </div>
         </div>
     </div>
 );
--- src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
+++ src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import styles from './UpcomingDaysForecastItem.module.css';
 
-const UpcomingDaysForecastItem = () => (
-    <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}></li>
+const imgUrlBase = 'https://www.metaweather.com/static/'
+
+const UpcomingDaysForecastItem = ({weekday, temperature, imgUrl}) => (
+    <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}>
+        <img className="mb-2" width="30" src={`${imgUrlBase}img/weather/${imgUrl}.svg`} alt="" />
+        <span className="mb-2">{weekday}</span>
+        <span className="font-weight-bold">{temperature}&deg;</span>
+    </li>
 );
 
 export default UpcomingDaysForecastItem;
